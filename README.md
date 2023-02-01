c366d1fb23467020b03462fcf1053b8fd3f35a4a
Eric Petsopoulos <ericpetsopoulos@Erics-MBP-2023.axia-art.com>
2023-02-01 22:45:18 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# stable-diffusion-mac-background-image-generator
-About Run a local instance of stable diffusion on your Mac that automatically creates and sets its own desktop background.
+# dAIly-frame
+Run a local instance of stable diffusion on your Mac that automatically creates and sets its own desktop background.
+
+## Setup:
+
+1. [Follow these steps to setup the HuggingFace Hub CLI on your machine.](https://huggingface.co/docs/huggingface_hub/quick-start)
+
+2. Install the required dependencies:
+```
+pip install diffusers transformers ftfy accelerate appscript git+https://github.com/sberbank-ai/Real-ESRGAN.git 
+```
+3. run the script 🚀
+
+## Examples:
+
+<img src="https://drive.google.com/uc?export=view&id=1kDi0cFNlai6swbqR15uJCXhOx6VRp_HM" style="width: 892px; max-width: 100%; height: auto" title="Click to enlarge picture" />
+<img src="https://drive.google.com/uc?export=view&id=1yxKiYYKeI7vXAm2inEXMDJ_amov5_Csn" style="width: 892px; max-width: 100%; height: auto" title="Click to enlarge picture" />
+<img src="https://drive.google.com/uc?export=view&id=1FbcPzU_Pc0ynb6_N3XMcGfjngNNv8PvD" style="width: 892px; max-width: 100%; height: auto" title="Click to enlarge picture" />
+<img src="https://drive.google.com/uc?export=view&id=1MaCX9iGuWzjJ4iQcWvDUoBKzll0605jk" style="width: 892px; max-width: 100%; height: auto" title="Click to enlarge picture" />
+
---
+++ generate_image.py
@@ -0,0 +1,100 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+import warnings
+import string
+import random
+import torch
+from diffusers import StableDiffusionPipeline
+from transformers import pipeline
+from RealESRGAN import RealESRGAN
+from appscript import app, mactypes
+
+# todo:
+# get laptop screen dimensions, are they divisible by 8?
+# put all functions in a utils file
+# windows support?
+# run from cli w/ a cmd?
+# try a different upscaling model
+
+def generate_prompt():
+    print('Step 1: generating image prompt... ⏳')
+    generator = pipeline('text-generation', model="mrm8488/bloom-560m-finetuned-sd-prompts")
+    scene_close_natural_feature_seeds = ["serene lake", "peaceful river", "radiant pond", "tranquil island"]
+    scene_manmade_structure_seeds = ["bridge", "cathedral", "skyscraper", "pagoda", "glass dome", "cabin", "amphitheatre", "hut", "tent", "greenhouse", "church", "mosque", "chapel", "boat", "mansion", "treehouse", "altar", "temple", "ruin", "castle", "plane", "hot air balloon"]
+    scene_setting = ["forest", "jungle", "park"]
+    scene_distant_natural_feature_seeds = ["mountains", "the moon", "planets", "stars", "lightning", "the sun"]
+    prompt_seed = f"highly detailed matte oil painting of a {random.choice(scene_close_natural_feature_seeds)} with a {random.choice(scene_manmade_structure_seeds)} in a verdant {random.choice(scene_setting)} with beautiful huge trees, an inspiring blue sky with impressive clouds and {random.choice(scene_distant_natural_feature_seeds)} in the distance"
+    prompt = generator(prompt_seed, max_length=77, num_return_sequences=1)
+    prompt = prompt[0]['generated_text']
+    print(f"Done ✅")
+    print(f"🗣️ Prompt: {prompt}")
+    print("")
+    return prompt
+
+def get_device():
+    if(torch.cuda.is_available()):
+        return "cuda"
+    elif(torch.backends.mps.is_available()):
+        return "mps"
+    else:
+        return "cpu"
+
+def generate_image(prompt):
+    model_id = "CompVis/stable-diffusion-v1-4" # "CompVis/stable-diffusion-v1-4", "stabilityai/stable-diffusion-2"
+    num_inference_steps = 50
+    guidance_scale = 7.5 
+    image_height = 768
+    image_width = 1024
+
+    print('Step 2: generating image... ⏳')
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, use_auth_token=True)
+    pipe = pipe.to(get_device())
+    pipe.enable_attention_slicing()
+    # ? kwargs not reached
+    pipe.safety_checker = lambda images, **kwargs: (images, False)
+    image = pipe(prompt, guidance_scale=guidance_scale, height=image_height, width=image_width, num_inference_steps=num_inference_steps).images[0]
+    print(f"Done ✅ ")
+    print("")
+    return image
+
+def upscale_image(image):
+    print('Step 3: upscaling image... ⏳')
+    device = torch.device(get_device())
+    model = RealESRGAN(device, scale=4)
+    model.load_weights(f"weights/RealESRGAN_x4.pth", download=True)
+    upscaled_image = model.predict(image)
+    print('Done ✅ ')
+    print("")
+    return upscaled_image
+
+def save_image(image, image_name, image_path):
+    print('Step 4: saving image... ⏳')
+    image.save(image_path)
+    print(f"{image_name}.png saved 💾")
+    print("")
+
+def set_desktop_bg(image_path):
+    print('Step 5: setting image to BG... ⏳ ')
+    app('Finder').desktop_picture.set(mactypes.File(f"{image_path}"))
+    print('Done ✅ ')
+    print("")
+
+def create_and_set_new_bg_image():
+    print("")
+    print("CREATING NEW BACKGROUND IMAGE 🎨")
+    print("")
+    image_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+    image_path = f"./images/{image_name}.png"
+    image_prompt = generate_prompt()
+    image = generate_image(image_prompt)
+    upscaled_image = upscale_image(image)
+    save_image(upscaled_image,image_name, image_path)
+    set_desktop_bg(image_path)
+    print("")
+    print("NEW BG SET 🚀 ")
+    print("")
+
+warnings.filterwarnings("ignore")
+create_and_set_new_bg_image()
